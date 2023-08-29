a56ec9369460d0e00acfcf11ba4ec875e04bb9ee
anton0380 <aa@unelma.ru>
2023-08-29 18:34:17 +0300

    bboard: create edit and delete bb

--- bboard/app/Http/Controllers/HomeController.php
+++ bboard/app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Bb;
 
 class HomeController extends Controller
 {
@@ -26,5 +27,37 @@ class HomeController extends Controller
     {
         return view('home',
                     ['bbs' => Auth::user()->bbs()->latest()->get()]);
+    }
+
+    public function create() {
+        return view('bb-create');
+    }
+
+    public function store(Request $request) {
+        Auth::user()->bbs()->create(['title' => $request->title,
+                                     'content' => $request->content,
+                                     'price' => $request->price]);
+        return redirect()->route('home');
+    }
+
+    public function edit(Bb $bb) {
+        return view('bb-edit', ['bb' => $bb]);
+    }
+
+    public function update(Request $request, Bb $bb) {
+        $bb->fill(['title' => $request->title,
+                   'content' => $request->content,
+                   'price' => $request->price]);
+        $bb->save();
+        return redirect()->route('home');
+    }
+
+    public function delete(Bb $bb) {
+        return view('bb-delete', ['bb' => $bb]);
+    }
+
+    public function destroy(Bb $bb) {
+        $bb->delete();
+        return redirect()->route('home');
     }
 }
